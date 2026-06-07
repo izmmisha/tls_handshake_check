a349192765909d133c3a4de293efa626ac54fe03
Mikhail I. Izmestev <izmmishao5@gmail.com>
2026-06-07 14:11:11 +0400

    tune default output format

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 def pytest_addoption(parser):
     parser.addoption("--port", type=int, default=443, help="TCP port number to connect to (default: 443)")
     parser.addoption("--sni", type=str, required=True, help="SNI used in clientHello")
@@ -11,3 +13,13 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("fix_sni", [metafunc.config.getoption("sni")])
     if "host" in metafunc.fixturenames:
         metafunc.parametrize("host", [metafunc.config.getoption("host")])
+
+@pytest.hookimpl(wrapper=True)
+def pytest_runtest_makereport(item, call):
+    report = yield
+    if report.when == "call":
+        terminalreporter = item.config.pluginmanager.get_plugin("terminalreporter")
+        if terminalreporter:
+            duration_text = f"[{report.duration:.4f}s] "
+            terminalreporter._tw.write(duration_text, cyan=True)
+    return report
